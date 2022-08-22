268df0856baf2e765841a6e00eac85a4a4c44874
meejyu <jua04016@naver.com>
2022-08-22 16:00:49 +0900

    배포 시 어떤 포트를 사용할 것인지 판단해주는 API

--- SpringBootAws/src/main/java/com/springboot/study/config/auth/SecurityConfig.java
+++ SpringBootAws/src/main/java/com/springboot/study/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable()// h2-console 화면을 사용하기 위해 해당옵션들을 disable 한다
                 .and()
                     .authorizeRequests() // URL별 권한 관리를 설정하는 옵션
-                    .antMatchers("/","/css/**","/images/**","/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/","/css/**","/images/**","/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole((Role.USER.name()))
                     .anyRequest().authenticated()
                 .and()
---
+++ SpringBootAws/src/main/java/com/springboot/study/web/ProfileController.java
@@ -0,0 +1,32 @@
+package com.springboot.study.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+
+        // 현재 실행중인 ActiveProfile을 모두 가져온다. real, real, real2는 모두 배포에 사용될 profile 이 중 하나라도 있으면 그 값을 반환하도록 한다.
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+
+        List<String> realProfiles = Arrays.asList("real", "real1", "real2");
+
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+    }
+
+
+
+}
